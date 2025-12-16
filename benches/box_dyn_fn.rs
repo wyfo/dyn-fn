be30e97577e9b683a42f4445b020367d4662ed2b
Joseph Perez <joperez@hotmail.fr>
2025-12-16 16:05:17 +0100

    Add a benchmark comparing Box<dyn Fn>

---
+++ benches/box_dyn_fn.rs
@@ -0,0 +1,31 @@
+use std::hint::black_box;
+
+use divan::Bencher;
+use dyn_fn::{LocalDynFn, storage};
+use higher_kinded_types::{ForFixed, ForRef};
+
+#[divan::bench]
+fn box_dyn_fn(b: Bencher) {
+    let f = black_box(Box::new(|s: &str| s.len()) as Box<dyn Fn(&str) -> usize>);
+    b.bench_local(|| f("test"))
+}
+
+#[divan::bench]
+fn dyn_fn(b: Bencher) {
+    let f = black_box(LocalDynFn::<ForRef<str>, ForFixed<usize>>::new(
+        |s: &str, _| s.len(),
+    ));
+    b.bench_local(|| f.call("test"))
+}
+
+#[divan::bench]
+fn dyn_fn_box(b: Bencher) {
+    let f = black_box(
+        LocalDynFn::<ForRef<str>, ForFixed<usize>, storage::Box>::new(|s: &str, _| s.len()),
+    );
+    b.bench_local(|| f.call("test"))
+}
+
+fn main() {
+    divan::main();
+}
