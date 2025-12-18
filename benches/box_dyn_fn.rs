6e2f98fd75e924e43a0d06cb838fd0c1ceb40bc3
Joseph Perez <joperez@hotmail.fr>
2025-12-18 15:39:31 +0100

    Remove useless benchmark

--- benches/box_dyn_fn.rs
+++ benches/box_dyn_fn.rs
@@ -1,7 +1,7 @@
 use std::hint::black_box;
 
 use divan::Bencher;
-use dyn_fn::{LocalDynFn, storage};
+use dyn_fn::LocalDynFn;
 use higher_kinded_types::{ForFixed, ForRef};
 
 #[divan::bench]
@@ -15,14 +15,6 @@ fn dyn_fn(b: Bencher) {
     let f = black_box(LocalDynFn::<ForRef<str>, ForFixed<usize>>::new(
         |s: &str, _| s.len(),
     ));
-    b.bench_local(|| f.call("test"))
-}
-
-#[divan::bench]
-fn dyn_fn_box(b: Bencher) {
-    let f = black_box(
-        LocalDynFn::<ForRef<str>, ForFixed<usize>, storage::Box>::new(|s: &str, _| s.len()),
-    );
     b.bench_local(|| f.call("test"))
 }
 
